c0ee540eb78b7da95115d6ef0411ae7d38629903
RyanKuehfuss <127149364+RyanKuehfuss@users.noreply.github.com>
2024-04-08 16:02:50 -0400

    Update protection_algorithm.py

--- src/protection_algorithm.py
+++ src/protection_algorithm.py
@@ -73,81 +73,85 @@ my_ip = get_ip_address()
 def extract_features(data):
     eth_length = 14
 
-    # Extract ethernet header
-    eth_header = data[:eth_length]
-    eth = unpack('!6s6sH', eth_header)
-    eth_protocol = socket.ntohs(eth[2])
-
-    # Parse IP packets only
-    if eth_protocol == 8:
-        # Parse IP header
-        ip_header = data[eth_length:20+eth_length]
-        iph = unpack('!BBHHHBBH4s4s', ip_header)
-        version_ihl = iph[0]
-        ihl = version_ihl & 0xF
-        iph_length = ihl * 4
-
-        # Extract IP source and destination addresses
-        src_ip = inet_ntoa(iph[8])
-        dst_ip = inet_ntoa(iph[9])
-
-        # Parse protocol
-        protocol = PROTOCOL_MAP.get(iph[6], "Unknown")
-
-        # Calculate packet size
-        packet_size = len(data)
-
-        # Get current timestamp
-        timestamp = datetime.datetime.now()
-        seconds = timestamp.second
-        microseconds = timestamp.microsecond
-
-        # Set Row Data
-        row_data = None
-
-        #Set appropriate header string to separate packet types
-        if iph[6] == 6:
-            csv_filename_prefix = "tcp_"
-        elif iph[6] == 17:
-            csv_filename_prefix = "udp_"
-        elif iph[6] == 1:
-            csv_filename_prefix = "icmp_"
-        else:
-            csv_filename_prefix = ""
-
-        # Parse TCP packets
-        if iph [6] == 6:
-            tcp_header = data[iph_length+eth_length:iph_length+eth_length+20]
-            tcph = unpack('!HHLLBBHHH', tcp_header)
-            flags = {
-                "FIN": (tcph[5] & 0x01) != 0,
-                "SYN": (tcph[5] & 0x02) != 0,
-                "RST": (tcph[5] & 0x04) != 0,
-                "PSH": (tcph[5] & 0x08) != 0,
-                "ACK": (tcph[5] & 0x10) != 0,
-                "URG": (tcph[5] & 0x20) != 0,
-                "ECE": (tcph[5] & 0x40) != 0,
-                "CWR": (tcph[5] & 0x80) != 0
-            }
-            src_port = tcph[0]
-            dst_port = tcph[1]
-            packet_data = [src_ip, src_port, dst_ip, dst_port, protocol, packet_size, seconds, microseconds] + list(flags.values())
-        # Parse UDP packets
-        elif iph[6] == 17:
-            udp_header = data[iph_length+eth_length:iph_length+eth_length+8]
-            udph = unpack('!HHHH', udp_header)
-            src_port = udph[0]
-            dst_port = udph[1]
-            packet_data = [src_ip, src_port, dst_ip, dst_port, protocol, packet_size, seconds, microseconds]
-        # Parse ICMP packets
-        elif iph[6] == 1:
-            icmp_header = data[iph_length+eth_length:iph_length+eth_length+4]
-            icmph = unpack('!BBH', icmp_header)
-            icmp_type = icmph[0]
-            packet_data = [src_ip, dst_ip, protocol, packet_size, seconds, microseconds, icmp_type]
-
-        print(protocol)
-        return np.array(packet_data), protocol
+    try:
+            # Extract ethernet header
+            eth_header = data[:eth_length]
+            eth = unpack('!6s6sH', eth_header)
+            eth_protocol = socket.ntohs(eth[2])
+        
+            # Parse IP packets only
+            if eth_protocol == 8:
+                # Parse IP header
+                ip_header = data[eth_length:20+eth_length]
+                iph = unpack('!BBHHHBBH4s4s', ip_header)
+                version_ihl = iph[0]
+                ihl = version_ihl & 0xF
+                iph_length = ihl * 4
+        
+                # Extract IP source and destination addresses
+                src_ip = inet_ntoa(iph[8])
+                dst_ip = inet_ntoa(iph[9])
+        
+                # Parse protocol
+                protocol = PROTOCOL_MAP.get(iph[6], "Unknown")
+        
+                # Calculate packet size
+                packet_size = len(data)
+        
+                # Get current timestamp
+                timestamp = datetime.datetime.now()
+                seconds = timestamp.second
+                microseconds = timestamp.microsecond
+        
+                # Set Row Data
+                row_data = None
+        
+                #Set appropriate header string to separate packet types
+                if iph[6] == 6:
+                    csv_filename_prefix = "tcp_"
+                elif iph[6] == 17:
+                    csv_filename_prefix = "udp_"
+                elif iph[6] == 1:
+                    csv_filename_prefix = "icmp_"
+                else:
+                    csv_filename_prefix = ""
+        
+                # Parse TCP packets
+                if iph [6] == 6:
+                    tcp_header = data[iph_length+eth_length:iph_length+eth_length+20]
+                    tcph = unpack('!HHLLBBHHH', tcp_header)
+                    flags = {
+                        "FIN": (tcph[5] & 0x01) != 0,
+                        "SYN": (tcph[5] & 0x02) != 0,
+                        "RST": (tcph[5] & 0x04) != 0,
+                        "PSH": (tcph[5] & 0x08) != 0,
+                        "ACK": (tcph[5] & 0x10) != 0,
+                        "URG": (tcph[5] & 0x20) != 0,
+                        "ECE": (tcph[5] & 0x40) != 0,
+                        "CWR": (tcph[5] & 0x80) != 0
+                    }
+                    src_port = tcph[0]
+                    dst_port = tcph[1]
+                    packet_data = [src_ip, src_port, dst_ip, dst_port, protocol, packet_size, seconds, microseconds] + list(flags.values())
+                # Parse UDP packets
+                elif iph[6] == 17:
+                    udp_header = data[iph_length+eth_length:iph_length+eth_length+8]
+                    udph = unpack('!HHHH', udp_header)
+                    src_port = udph[0]
+                    dst_port = udph[1]
+                    packet_data = [src_ip, src_port, dst_ip, dst_port, protocol, packet_size, seconds, microseconds]
+                # Parse ICMP packets
+                elif iph[6] == 1:
+                    icmp_header = data[iph_length+eth_length:iph_length+eth_length+4]
+                    icmph = unpack('!BBH', icmp_header)
+                    icmp_type = icmph[0]
+                    packet_data = [src_ip, dst_ip, protocol, packet_size, seconds, microseconds, icmp_type]
+        
+                return np.array(packet_data), protocol
+    except:
+        logging.error("Error extracting features:", e)
+        # If an exception occurs, return default values
+        return np.array([]), "Unknown"
 
 # Function to block traffic from an IP address
 def block_ip(ip_address, protocol):
